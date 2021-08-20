e058bc5f0a765c92d0592bd45fc96762c3c4420f
Lucas Araujo <lucasaraujo4188@gmail.com>
2021-08-19 23:08:29 -0300

    Adiciona o script de definição de dados (DDL)

---
+++ Banco de dados/scripts/spmedgroup_01_DDL.sql
@@ -0,0 +1,78 @@
+CREATE DATABASE SPMEDGROUP;
+GO
+
+USE SPMEDGROUP;
+GO
+
+CREATE TABLE tipoUsuario (
+	idTipoUsuario TINYINT PRIMARY KEY IDENTITY,
+	nomeTipoUsuario VARCHAR(70) UNIQUE
+);
+GO
+
+CREATE TABLE clinica (
+	idClinica SMALLINT PRIMARY KEY IDENTITY,
+	endClinica VARCHAR(200) NOT NULL,
+	cnpj CHAR(14) UNIQUE NOT NULL, 
+	nomeFantasia VARCHAR(100) NOT NULL,
+	razaoSocial VARCHAR(100) UNIQUE NOT NULL,
+	horaAbertura TIME,
+	horaFechamento TIME
+);
+GO
+
+CREATE TABLE especialidade (
+	idEspecialidade TINYINT PRIMARY KEY IDENTITY,
+	nomeEspecialidade VARCHAR(100) UNIQUE
+);
+GO
+
+CREATE TABLE situacao (
+	idSituacao TINYINT PRIMARY KEY IDENTITY,
+	situacaoDesc VARCHAR(100)
+);
+GO
+
+CREATE TABLE usuario (
+	idUsuario INT PRIMARY KEY IDENTITY,
+	idTipoUsuario TINYINT FOREIGN KEY REFERENCES tipoUsuario(idTipoUsuario) NOT NULL,
+	email VARCHAR(50) UNIQUE NOT NULL,
+	senha VARCHAR(20) NOT NULL
+);
+GO
+
+CREATE TABLE paciente (
+	idPaciente INT PRIMARY KEY IDENTITY,
+	idUsuario INT FOREIGN KEY REFERENCES usuario(idUsuario) UNIQUE NOT NULL,
+	nomePaciente VARCHAR(100) NOT NULL,
+	rgPaciente CHAR(9) UNIQUE NOT NULL,
+	cpfPaciente CHAR(11) UNIQUE NOT NULL, 
+	endPaciente VARCHAR(200) NOT NULL,
+	dataNascPaciente DATE NOT NULL,
+	telPaciente VARCHAR(11)
+);
+GO
+
+CREATE TABLE medico (
+	idMedico SMALLINT PRIMARY KEY IDENTITY,
+	idUsuario INT FOREIGN KEY REFERENCES usuario(idUsuario) UNIQUE NOT NULL,
+	idClinica SMALLINT FOREIGN KEY REFERENCES clinica(idClinica) NOT NULL,
+	idEspecialidade TINYINT FOREIGN KEY REFERENCES especialidade(idEspecialidade) NOT NULL,
+	crm CHAR(7) UNIQUE NOT NULL,
+	nomeMedico VARCHAR(100) NOT NULL
+);
+GO
+
+CREATE TABLE consulta (
+	idConsulta INT PRIMARY KEY IDENTITY,
+	idPaciente INT FOREIGN KEY REFERENCES paciente(idPaciente) NOT NULL,
+	idMedico SMALLINT FOREIGN KEY REFERENCES medico(idMedico) NOT NULL,
+	idSituacao TINYINT FOREIGN KEY REFERENCES situacao(idSituacao) NOT NULL,
+	consultaDesc VARCHAR(500),
+	dataConsulta DATETIME NOT NULL,
+);
+GO
+
+
+
+
