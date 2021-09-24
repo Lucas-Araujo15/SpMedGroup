5cfcb0bef50d9f031e985adac8b72e4aa26c224f
Lucas Araujo <lucasaraujo4188@gmail.com>
2021-09-24 11:20:39 -0300

    Inicia a construção dos repositories

---
+++ Back-end/spmedgroup.webAPI/spmedgroup.webAPI/Repositories/ClinicaRepository.cs
@@ -0,0 +1,37 @@
+﻿using spmedgroup.webAPI.Domains;
+using spmedgroup.webAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace spmedgroup.webAPI.Repositories
+{
+    public class ClinicaRepository : IClinicaRepository
+    {
+        public void Atualizar(int idClinica, Clinica novaClinica)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Clinica BuscarPorId(int idClinica)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cadastrar(Clinica clinica)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deletar(int idClinica)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Clinica> ListarTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Back-end/spmedgroup.webAPI/spmedgroup.webAPI/Repositories/ConsultaRepository.cs
@@ -0,0 +1,47 @@
+﻿using spmedgroup.webAPI.Domains;
+using spmedgroup.webAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace spmedgroup.webAPI.Repositories
+{
+    public class ConsultaRepository : IConsultaRepository
+    {
+        public void Aprovacao(int idConsulta, string status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualizar(int idConsulta, Consultum novaConsulta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Consultum BuscarPorId(int idConsulta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cadastrar(Consultum consulta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deletar(int idConsulta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Consultum> ListarMinhas(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Consultum> ListarTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
